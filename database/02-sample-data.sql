8985692f4490c8280dca4e92d0cda7169198669a
Krzysztof Podurgiel <s17769@pjwstk.edu.pl>
2020-05-18 01:03:23 +0200

    cw4 » 4.1.2 » przykładowe dane

---
+++ database/02-sample-data.sql
@@ -0,0 +1,10 @@
+INSERT INTO Studies VALUES
+(1, 'Informatyka');
+
+INSERT INTO Enrollment VALUES
+(1, 1, 1, '2019-10-01');
+
+INSERT INTO Student VALUES
+('S11111', 'Jan',   'Kowalski',  '1991-01-01', 1),
+('S11112', 'Kamil', 'Nowak',     '1992-01-02', 1),
+('S11113', 'Piotr', 'Kowalczyk', '1993-01-03', 1);
